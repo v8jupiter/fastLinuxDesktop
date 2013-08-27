42f3e51de4eb15571132b0b8dccfb5f49876be5f
vladislav <vladbbk@gmail.com>
2013-08-27 16:44:43 +0300

    upload configs

---
+++ openbox/autostart.sh
@@ -0,0 +1,9 @@
+setxkbmap -layout us,ru -variant -option grp:ctrl_shift_toggle,grp_led:scroll,terminate:ctrl_alt_bksp &
+tint2 &
+xxkb &
+feh --bg-center /home/vladwork/desktop/Business_wallpapers_275.jpg &
+volumeicon &
+google-chrome &
+skype &
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+eval $(shell gnome-keyring-daemon -s) &
